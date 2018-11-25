40c3c32878ab6ef664a0801c6a992477f3276513
Cheng C Yang <Cheng C Yang@cn.asia.ad.pwcinternal.com>
2018-11-25 17:24:40 +0800

    cheng

---
+++ OnedayCP2/back/src/main/java/Draft.java
@@ -0,0 +1,15 @@
+import java.math.BigDecimal;
+
+public class Draft {
+
+    public static void main(String[] args) {
+
+        short a = 666;
+        if (a == 666){
+            System.out.println(999);
+        }
+
+    }
+
+
+}
---
+++ OnedayCP2/back/src/main/java/com/ethermorgan/onedaycp/OnedaycpApplication.java
@@ -0,0 +1,12 @@
+package com.ethermorgan.onedaycp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnedaycpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnedaycpApplication.class, args);
+    }
+}
